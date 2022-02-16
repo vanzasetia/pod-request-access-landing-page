1c5d435e23648bc6074cc92cd4d65ce66401e1ea
Vanza Setia <venusbumi2@gmail.com>
2022-02-16 18:03:29 +0700

    Add custom form validation
    This commit allows the user to receive an alert message when the form is submitted if:
    - The Email address field is empty, it will show "Oops! Please add your email"
    - The email is not formatted correctly, it will show "Oops! Please check your email"

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
 (function () {
-  "use strict";
-  console.log("Hello World");
+  'use strict';
+  const heroForm = document.querySelector('.js-heroForm');
+  const heroInput = document.querySelector('.js-heroInput');
+  const heroAlert = document.querySelector('.js-heroAlert');
+
+  const showAlertMessage = alertMessage => {
+    heroAlert.textContent = alertMessage;
+    heroAlert.removeAttribute('hidden');
+    heroAlert.setAttribute('role', 'alert');
+  };
+
+  const hideAlertMessage = () => {
+    heroAlert.textContent = '';
+    heroAlert.setAttribute('hidden', '');
+    heroAlert.removeAttribute('role');
+  };
+
+  const validateEmail = event => {
+    const emailValidation =
+      /^(?:[a-z0-9.]){2,30}@{1}(?:[a-z0-9-]){2,30}\.{1}(?:[a-z0-9]){2,3}(?:\.(?:[a-z0-9]){2,3})?$/;
+    const isEmailInvalid = !emailValidation.test(heroInput.value);
+    const message =
+      isEmailInvalid && heroInput.value
+        ? 'Oops! Please check your email'
+        : 'Oops! Please add your email';
+    if (isEmailInvalid) {
+      event.preventDefault();
+      if (heroInput.value) {
+        showAlertMessage(message);
+        setTimeout(hideAlertMessage, 5000);
+      } else {
+        showAlertMessage(message);
+        setTimeout(hideAlertMessage, 5000);
+      }
+    }
+  };
+
+  heroForm.addEventListener('submit', validateEmail);
 })();
